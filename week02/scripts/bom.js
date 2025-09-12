6ad526d5c0c5e69d42dfbaee2a7c7e9f5bea6fc5
r-benjaber <polbenjaber@gmail.com>
2025-09-12 16:27:22 -0300

    Finished bom assignment

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,10 +2,25 @@ const btn = document.querySelector("button");
 const inp = document.querySelector("#favchap");
 const list = document.querySelector("#list");
 
-const book = document.createElement("li");
-const del = document.createElement("button");
 
-book.textContent = inp.value;
-del.textContent = "❌";
-book.append(del);
-list.append(book);
+
+btn.addEventListener("click", function() {
+    if (inp.value.trim() !== "") {
+        const book = document.createElement("li");
+        const del = document.createElement("button");
+
+        book.textContent = inp.value;
+        del.textContent = "❌";
+                
+        book.append(del);
+        list.append(book);
+
+        del.addEventListener("click", function () {
+            list.removeChild(book);
+            inp.focus();
+        })
+    }
+    inp.focus();
+    inp.value = ""
+});
+
